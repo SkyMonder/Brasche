06282f47b897fe5725a6472c4d547fca6e839d78
SkyMonder <rilloperdagelo@gmail.com>
2026-04-22 22:56:21 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
 
-echo "=== Установка GNU Chess ==="
-# Устанавливаем движок и книгу дебютов из официального репозитория.
-# Это гарантирует установку последней версии, совместимой с вашим окружением.
-apt-get update && apt-get install -y gnuchess gnuchess-book
-
-# Копируем бинарный файл в корень, чтобы наш Python-код мог его найти.
-cp /usr/games/gnuchess ./engine
-chmod +x ./engine
-
-echo "=== Запуск GNU Chess ==="
+echo "=== Установка Stockfish 18 ==="
+mkdir -p temp
+cd temp
+wget -q https://github.com/official-stockfish/Stockfish/releases/download/sf_18/stockfish-ubuntu-x86-64-bmi2.tar
+tar -xf stockfish-ubuntu-x86-64-bmi2.tar
+cp stockfish/stockfish-ubuntu-x86-64-bmi2 ../stockfish
+cd ..
+rm -rf temp
+chmod +x ./stockfish
 exec gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:$PORT engine:app
