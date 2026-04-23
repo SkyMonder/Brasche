58b5d8c6fd91e82deb726b077fa4cbf396f38f2c
SkyMonder <rilloperdagelo@gmail.com>
2026-04-23 19:59:30 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 set -e
-echo "=== Запуск API-сервиса ChessDB ==="
+
+echo "=== Установка Stockfish 18 ==="
+mkdir -p temp
+cd temp
+wget -q https://github.com/official-stockfish/Stockfish/releases/download/sf_18/stockfish-ubuntu-x86-64-bmi2.tar
+tar -xf stockfish-ubuntu-x86-64-bmi2.tar
+cp stockfish/stockfish-ubuntu-x86-64-bmi2 ../engine
+cd ..
+rm -rf temp
+chmod +x ./engine
+
 exec gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:$PORT engine:app
